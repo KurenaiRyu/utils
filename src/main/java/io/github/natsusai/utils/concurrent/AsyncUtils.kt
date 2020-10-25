881b2a5d82b67ece14c48117485783fa85573c8f
Kurenai <kurenai233@yahoo.com>
2020-10-25 18:38:16 +0800

    perf: 去掉不必要的方法

--- src/main/java/io/github/natsusai/utils/concurrent/AsyncUtils.kt
+++ src/main/java/io/github/natsusai/utils/concurrent/AsyncUtils.kt
@@ -1,6 +1,5 @@
 package io.github.natsusai.utils.concurrent
 
-import java.util.*
 import java.util.concurrent.*
 
 /**
@@ -24,7 +23,7 @@ class AsyncUtils {
         </R> */
         @Throws(Exception::class)
         fun <R> submit(timeout: Long, timeUnit: TimeUnit, task: Callable<R>): R? {
-            val future = FutureTask { doTask(task) }
+            val future = FutureTask(task)
             Thread(future).start()
             return getResult(future, timeout, timeUnit)
         }
@@ -42,39 +41,8 @@ class AsyncUtils {
         </R> */
         @Throws(Exception::class)
         fun <R> submit(timeout: Long, timeUnit: TimeUnit, executor: ExecutorService, task: Callable<R>): R? {
-            val future = executor.submit<R> { doTask(task) }
+            val future = executor.submit(task)
             return getResult(future, timeout, timeUnit)
-        }
-
-        /**
-         * 创建一个新线程执行传入的方法并在指定时间等待执行完成
-         *
-         * @param timeout  超时时间
-         * @param timeUnit 时间单位
-         * @param task     被调用的方法
-         * @throws Exception 执行被调用方法异常
-         */
-        @Throws(Exception::class)
-        fun execute(timeout: Long, timeUnit: TimeUnit, task: Task) {
-            val future = FutureTask { doTask(task) }
-            Thread(future).start()
-            getResult(future, timeout, timeUnit)
-        }
-
-        /**
-         * 调用线程池执行传入的方法并在指定时间等待执行完成
-         *
-         * @param timeout  超时时间
-         * @param timeUnit 时间单位
-         * @param executor 线程池对象
-         * @param task     被调用的方法
-         * @throws Exception 执行被调用方法异常
-         */
-        @Throws(Exception::class)
-        fun execute(timeout: Long, timeUnit: TimeUnit, executor: ExecutorService?, task: Task) {
-            assert(executor != null)
-            val future = executor!!.submit<Any?> { doTask(task) }
-            getResult(future, timeout, timeUnit)
         }
 
         /**
@@ -93,32 +61,6 @@ class AsyncUtils {
             } catch (e: TimeoutException) {
                 null
             }
-        }
-
-        /**
-         * 执行任务
-         *
-         * @param task 任务
-         * @return 异常或是执行结果对象
-         */
-        private fun <R> doTask(task: Callable<R>): R {
-            return task.call()
-        }
-
-        /**
-         * 执行任务
-         *
-         * @param task 任务
-         * @return 异常对象
-         */
-        private fun doTask(task: Task): Any? {
-            task.run()
-            return null
-        }
-
-        fun interface Task {
-            @Throws(Exception::class)
-            fun run()
         }
     }
 }
